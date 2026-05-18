f2e0274e9580f9f061f6b7cb3e1bdf18dc7f94b6
soo <dlgustn96@gmail.com>
2026-05-18 13:01:36 +0900

    feat: Toss 결제 승인 Api 실패시 응답 로그 추가
    Related to: #25

--- src/main/java/com/pagely/paymentservice/infrastructure/client/pg/TossPaymentFeignConfig.java
+++ src/main/java/com/pagely/paymentservice/infrastructure/client/pg/TossPaymentFeignConfig.java
@@ -1,5 +1,6 @@
 package com.pagely.paymentservice.infrastructure.client.pg;
 
+import feign.Logger;
 import feign.RequestInterceptor;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,5 +17,10 @@ public class TossPaymentFeignConfig {
             String encoded = Base64.getEncoder().encodeToString((secretKey + ":").getBytes());
             requestTemplate.header("Authorization", "Basic " + encoded);
         };
+    }
+
+    @Bean
+    public Logger.Level tossLoggerLevel() {
+        return Logger.Level.FULL;
     }
 }
